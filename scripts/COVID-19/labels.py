b27f18167daffa1925d6e39f57c3fe5b557938f1
andefined <dimitris.papaevagelou@andefined.com>
2020-05-12 14:06:35 +0300

    automated commit (Τρι 12 Μαΐ 2020 02:06:09 μμ EEST)

---
+++ scripts/COVID-19/labels.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+def scatter_country(lang):
+    if lang == "EL":
+        return "ADMIN_GR"
+    else:
+        return "Country,Other"
+
+
+def scatter_labels(lang):
+    if lang == "EL":
+        return {
+            "Deaths/1M pop": "<b>Θάνατοι</b>/<br>1Μ",
+            "Tests/ 1M pop": "Τεστ/ 1M πληθυσμού",
+            "Tot Cases/1M pop": "Κρούσματα/ 1M πληθυσμού",
+            "size": "",
+            "text": "",
+        }
+    else:
+        return {
+            "Deaths/1M pop": "<b>Deaths</b>/<br>1Μ",
+            "Tests/ 1M pop": "Tests/ 1M population",
+            "Tot Cases/1M pop": "Cases/ 1M population",
+            "size": "",
+            "text": "",
+        }
+
+
+def scatter_title(lang):
+    if lang == "EL":
+        return "<b>Κρούσματα</b>, <b>Τεστ</b> και <b>Θάνατοι</b> ανά χώρα<br>"
+    else:
+        return "<b>Cases</b>, <b>Tests</b> and <b>Deaths</b> per country"
+
+
+def scatter_xaxis_title(lang):
+    if lang == "EL":
+        return "<b>Τεστ</b>/1 εκατ. πληθυσμού"
+    else:
+        return "<b>Tests</b>/1M population"
+
+
+def scatter_yaxis_title(lang):
+    if lang == "EL":
+        return "<b>Κρούσματα</b>/1 εκατ. πληθυσμού"
+    else:
+        return "<b>Cases</b>/1M population"
+
+
+def line_button_all(lang):
+    if lang == "EL":
+        return "ΟΛΑ"
+    else:
+        return "ALL"
+
+
+def line_button_deaths(lang):
+    if lang == "EL":
+        return "Θάνατοι"
+    else:
+        return "Deaths"
+
+
+def line_button_intub(lang):
+    if lang == "EL":
+        return "Διασ/μένοι"
+    else:
+        return "Intubated"
+
+
+def line_button_all_title(lang):
+    if lang == "EL":
+        return "Θάνατοι και διασωληνωμένοι ασθενείς στην Ελλάδα"
+    else:
+        return "Deaths and intubated patients in Greece"
+
+
+def line_button_deaths_title(lang):
+    if lang == "EL":
+        return "Θάνατοι/ημέρα στην Ελλάδα"
+    else:
+        return "Deaths/day in Greece"
+
+
+def line_button_intub_title(lang):
+    if lang == "EL":
+        return "Διασωληνωμένοι ασθενείς/ ημέρα στην Ελλάδα"
+    else:
+        return "Intubated patients/day in Greece"
+
+
+def line_trace_deaths(lang):
+    if lang == "EL":
+        return "θάνατοι"
+    else:
+        return "deaths"
+
+
+def line_trace_intub(lang):
+    if lang == "EL":
+        return "διασωληνωμένοι"
+    else:
+        return "intubated"
+
+
+def regions_facets_title(lang):
+    if lang == "EL":
+        return "Εξέλιξη <b>θανάτων</b> ανά <b>Περιφέρεια</b>"
+    else:
+        return "Evolution of <b>deaths</b> by <b>district</b>"
+
+
+def regions_facets_xaxis_note(lang):
+    if lang == "EL":
+        return "<i>Δεν περιλαμβάνονται θάνατοι με άγνωστο γεωγραφικό προσδιορισμό.</i>"
+    else:
+        return "<i>Deaths with unknown location are not included.</i>"
+
+
+def regions_facets_labels(lang):
+    if lang == "EL":
+        return {
+            "district": "Περιφέρεια",
+            "Date": "Ημ/νία",
+            "deaths": "Θάνατοι συνολικά",
+        }
+    else:
+        return {
+            "district": "District",
+            "Date": "Date",
+            "deaths": "Total Deaths",
+        }
+
+
+def choropleth_casesrate_title(lang):
+    if lang == "EL":
+        return "<br><b>Κρούσματα</b> αναλογικά με τα τεστ ανά χώρα<br>"
+    else:
+        return "<br>Ratio of <b>cases</b> in tests per country"
+
+
+def choropleth_casesrate_annot(lang):
+    if lang == "EL":
+        return "Οι διαφορετικές πολιτικές των χωρών ως προς τα τεστ <br>μπορεί να οδηγούν σε υποεκτιμήσεις ή υπερεκτιμήσεις."
+    else:
+        return "Different policies of various countries on testing <br>might drive to underestimations or overestimations."
+
+
+def choropleth_recoveredrate_title(lang):
+    if lang == "EL":
+        return "<br>Όσοι <b>ανάρρωσαν</b> ανά χώρα<br>"
+    else:
+        return "<br><b>Recovery</b> ratio per country"
+
+
+def choropleth_recoveredrate_annot(lang):
+    if lang == "EL":
+        return "ανάρρωσαν/κρούσματα (%)<br>Οι διαφορετικές πολιτικές των χωρών ως προς την<br>καταγραφή των στοιχείων μπορεί να επηρεάζουν τα αποτελέσματα."
+    else:
+        return "recovered/cases (%)<br>Different policies of various countries on<br>record keeping might influence results."
