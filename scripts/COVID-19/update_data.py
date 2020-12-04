1f33b53381fcb87aad7052cc81506681b0bb1114
andefined <dimitris.papaevagelou@andefined.com>
2020-12-04 13:06:17 +0200

    update mech

--- scripts/COVID-19/update_data.py
+++ scripts/COVID-19/update_data.py
@@ -15,21 +15,21 @@ if __name__ == "__main__":
     root_path = args.root_path
 
     greeceTimeline_df = pd.read_csv(root_path + "/COVID-19/greeceTimeline.csv")
-    if pd.to_datetime(datetime.today()) != pd.to_datetime(greeceTimeline_df.columns[-1]):
-        this_date = pd.to_datetime(datetime.today()).strftime("%-m/%-d/%-y")
-        greeceTimeline_df[this_date] = greeceTimeline_df[greeceTimeline_df.columns[-1]]
-        greeceTimeline_df.at[0, this_date] = np.nan # cases skip
-        greeceTimeline_df.at[1, this_date] = np.nan # deaths skip
+    today = pd.to_datetime(datetime.today()).strftime("%-m/%-d/%-y")
+    last_date = pd.to_datetime(greeceTimeline_df.columns[-1]).strftime("%-m/%-d/%-y")
+    if today != last_date:
+        greeceTimeline_df[today] = greeceTimeline_df[last_date]
+        greeceTimeline_df.at[0, today] = np.nan # cases skip
+        greeceTimeline_df.at[1, today] = np.nan # deaths skip
         # recovered copy
-        greeceTimeline_df.at[3, this_date] = np.nan # hospitalized skip
-        greeceTimeline_df.at[4, this_date] = np.nan # intensive_care skip
+        greeceTimeline_df.at[3, today] = np.nan # hospitalized skip
+        greeceTimeline_df.at[4, today] = np.nan # intensive_care skip
         # intubated copy
         # cumulative_rtpcr_tests_raw copy
-        greeceTimeline_df.at[7, this_date] = np.nan # estimated_new_rtpcr_tests skip
+        greeceTimeline_df.at[7, today] = np.nan # estimated_new_rtpcr_tests skip
         # cumulative_rapid_tests_raw copy
-        greeceTimeline_df.at[9, this_date] = np.nan # esitmated_new_rapid_tests skip
-        greeceTimeline_df.at[10, this_date] = np.nan # estimated_new_total_tests skip
+        greeceTimeline_df.at[9, today] = np.nan # esitmated_new_rapid_tests skip
+        greeceTimeline_df.at[10, today] = np.nan # estimated_new_total_tests skip
         # total cases copy
-        # greeceTimeline_df.to_csv(root_path + "/COVID-19/greeceTimeline.csv", index=False)
-        print(greeceTimeline_df)
+        greeceTimeline_df.to_csv(root_path + "/COVID-19/greeceTimeline.csv", index=False)
     sys.exit(0)
